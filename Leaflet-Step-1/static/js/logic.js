3f46cf222c4695eae1165639bb25b57ce0ed38cb
alanseed <alan.seed@gmail.com>
2021-09-04 20:51:47 +1000

    Stage 1 done

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,6 +1,6 @@
 function getColor(d) {
-  return d > 200.0 ? "#7a0177" :
-    d > 100.0 ? "#BD0026" :
+  return d > 200.0 ? "#BD0000" :
+    d > 100.0 ? "#BD2F00" :
       d > 50.0 ? "#E31A1C" :
         d > 20.0 ? "#FC4E2A" :
           d > 10.0 ? "#FD8D3C" :
@@ -13,10 +13,14 @@ var legend = L.control({ position: "bottomright" }) ;
 
 legend.onAdd = function (map) {
   var div = L.DomUtil.create("div", "info legend");
+  var heading = L.DomUtil.create("h3","heading", div);
+  heading.innerHTML += "Depth (km)" ;
+  var ulist = L.DomUtil.create("ul", "legend", div);
   var grades = [1, 5, 10, 20, 50, 100, 200];
   for (var i = 0; i < grades.length; i++) {
-    div.innerHTML += `<li style="background:` + getColor(grades[i] + 1) + `"></li>` + grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    ulist.innerHTML += `<li style="background:` + getColor(grades[i] + 1) + `">` + grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+</li>');
   }
+  
   console.log(div);
   return div;
 };
@@ -78,48 +82,3 @@ function createMarkers(response) {
 
 // Perform an API call to the USGS to get earthquake information. Call createMarkers when complete
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(createMarkers);
-
-function legend_for_depth_layer(layer, name, units, id) {
-  // Generate a HTML legend for a Leaflet layer 
-  //
-  // Arguments:
-  // layer: The leaflet Layer object referring to the layer - must be a layer using
-  //        choropleth.js
-  // name: The name to display in the layer control (will be displayed above the legend, and next
-  //       to the checkbox
-  // units: A suffix to put after each numerical range in the layer - for example to specify the
-  //        units of the values - but could be used for other purposes)
-  // id: The id to give the <ul> element that is used to create the legend. Useful to allow the legend
-  //     to be shown/hidden programmatically
-  //
-  // Returns:
-  // The HTML ready to be used in the specification of the layers control
-  var limits = layer.options.limits;
-  var colors = layer.options.colors;
-  var labels = [];
-
-  // Start with just the name that you want displayed in the layer selector
-  var HTML = name
-
-  // For each limit value, create a string of the form 'X-Y'
-  limits.forEach(function (limit, index) {
-      if (index === 0) {
-          var to = parseFloat(limits[index]).toFixed(0);
-          var range_str = "< " + to;
-      }
-      else {
-          var from = parseFloat(limits[index - 1]).toFixed(0);
-          var to = parseFloat(limits[index]).toFixed(0);
-          var range_str = from + "-" + to;
-      }
-
-      // Put together a <li> element with the relevant classes, and the right colour and text
-      labels.push('<li class="sublegend-item"><div class="sublegend-color" style="background-color: ' +
-          colors[index] + '"> </div> ' + range_str + units + '</li>');
-  })
-
-  // Put all the <li> elements together in a <ul> element
-  HTML += '<ul id="' + id + '" class="sublegend">' + labels.join('') + '</ul>';
-
-  return HTML;
-}
